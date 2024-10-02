924f344d32542024c54bdbe234325e542331663c
Artur Lebedev <IinfernalI95@gmail.com>
2024-10-02 20:55:22 +0300

    Initial Commit

---
+++ JustChat JustChatTests/JustChat_JustChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustChat_JustChatTests.swift
+//  JustChat JustChatTests
+//
+//  Created by Артур Лебедев on 02.10.2024.
+//
+
+import Testing
+@testable import JustChat_JustChat
+
+struct JustChat_JustChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
